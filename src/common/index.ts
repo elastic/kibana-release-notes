12a907c914ac61633e228597a5eaf20d8be8d4c9
Tim Roes <mail@timroes.de>
2021-03-16 16:07:42 +0100

    Remove unused component

--- src/common/index.ts
+++ src/common/index.ts
@@ -4,6 +4,5 @@ export * from './pr-utils';
 export * from './output-utils';
 export { Pr } from './pr';
 export { validateToken } from './github';
-export { AsiidocRenderer } from './asciidoc';
 
 export type { TokenValidated } from './github';
