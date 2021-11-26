cb2002ce36d6ddb69127327f44306aaf1fc31215
Tim Roes <mail@timroes.de>
2021-11-26 11:24:10 +0100

    Improve navigation style

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,8 @@ import {
 } from '@elastic/eui';
 import { GITHUB_TOKEN } from './common';
 import { ApiChangesPage, GitHubSettings, ReleaseNotesPage } from './pages';
-import { Route, Routes, useNavigate, Navigate } from 'react-router-dom';
+import { Route, Routes, useNavigate, Navigate, useMatch } from 'react-router-dom';
+import { ReactComponent as GitHubIcon } from './github-icon.svg';
 
 export const App: React.FC = () => {
   const isGitHubTokenSet = !!localStorage.getItem(GITHUB_TOKEN);
@@ -33,8 +34,15 @@ export const App: React.FC = () => {
         </EuiHeaderSection>
         <EuiHeaderSection grow={true}>
           <EuiHeaderLinks>
-            <EuiHeaderLink onClick={() => navigate('/release-notes')}>Release Notes</EuiHeaderLink>
-            <EuiHeaderLink onClick={() => navigate('/devdocs')}>Kibana API changes</EuiHeaderLink>
+            <EuiHeaderLink
+              isActive={!!useMatch('/release-notes')}
+              onClick={() => navigate('/release-notes')}
+            >
+              Release Notes
+            </EuiHeaderLink>
+            <EuiHeaderLink isActive={!!useMatch('/devdocs')} onClick={() => navigate('/devdocs')}>
+              Kibana API changes
+            </EuiHeaderLink>
           </EuiHeaderLinks>
         </EuiHeaderSection>
         <EuiHeaderSection side="right">
@@ -43,7 +51,7 @@ export const App: React.FC = () => {
               aria-label="GitHub Settings"
               onClick={() => navigate('/github')}
             >
-              <EuiIcon type="gear" />
+              <EuiIcon type={GitHubIcon} size="l" />
             </EuiHeaderSectionItemButton>
           </EuiHeaderSectionItem>
         </EuiHeaderSection>
