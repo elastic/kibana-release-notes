2e193f5d423b7fabf8cc14221badd9b180ae1450
Brad White <brad.white@elastic.co>
2024-08-07 15:10:54 -0600

    add endpoint template

---
+++ src/config/templates/endpoint.ts
@@ -0,0 +1,89 @@
+import type { Config } from './types';
+
+export const endpointLabels = [
+  'feature:hostisolation',
+  'feature:memoryscan',
+  'feature:harden',
+  'feature:memoryprotection',
+  'feature:comms',
+  'feature:performance',
+  'feature:malware',
+  'feature:events',
+  'feature:install',
+  'feature:policy',
+  'feature:ransomware',
+  'feature:security',
+  'feature:testing',
+  'feature:rules',
+  'feature:ASR',
+  'feature:shipper',
+  'feature:code_quality',
+  'feature:false-positives',
+  'feature:filescore',
+  'feature:telemetry',
+  'feature:agentintegration',
+  'feature:user_experience',
+];
+
+export const endpointTemplate: Config = {
+  includedLabels: endpointLabels,
+  excludedLabels: ['backport', 'release_note:skip'],
+  areas: [
+    {
+      title: 'Elastic Endpoint',
+      labels: endpointLabels,
+    },
+  ],
+  templates: {
+    pages: {
+      releaseNotes: `[discrete]
+[[release-notes-{{version}}]]
+=== {{version}}
+{{#prs.breaking}}
+
+[discrete]
+[[breaking-changes-{{version}}]]
+==== Breaking changes
+{{{prs.breaking}}}
+{{/prs.breaking}}
+{{#prs.deprecations}}
+
+[discrete]
+[[deprecations-{{version}}]]
+==== Deprecations
+{{{prs.deprecations}}}
+{{/prs.deprecations}}
+{{#prs.features}}
+
+[discrete]
+[[features-{{version}}]]
+==== New features
+{{{prs.features}}}
+{{/prs.features}}
+{{#prs.enhancements}}
+
+[discrete]
+[[enhancements-{{version}}]]
+==== Enhancements
+{{{prs.enhancements}}}
+{{/prs.enhancements}}
+{{#prs.fixes}}
+
+[discrete]
+[[bug-fixes-{{version}}]]
+==== Bug fixes
+{{{prs.fixes}}}
+{{/prs.fixes}}
+
+`,
+    },
+    prGroup: '{{{prs}}}',
+    prs: {
+      breaking: `*{{{title}}}*\n\n!!TODO!!\n\nSee ({kibana-pull}{{number}}[#{{number}}]) for details.\n`,
+      deprecation: `*{{{title}}}*\n\n!!TODO!!\n\nSee ({kibana-pull}{{number}}[#{{number}}]) for details.\n`,
+      _other_:
+        '* {{{title}}} ({kibana-pull}{{number}}[#{{number}}]).' +
+        '{{#details}}\n////\n!!TODO!! The above PR had a lengthy release note description:\n{{{details}}}\n////{{/details}}',
+    },
+  },
+};
--- src/config/templates/index.ts
+++ src/config/templates/index.ts
@@ -1,19 +1,22 @@
 import { kibanaTemplate } from './kibana';
 import { securityTemplate } from './security';
+import { endpointTemplate } from './endpoint';
 import { Config } from './types';
+import type { EuiIconProps } from '@elastic/eui';
 
 export * from './types';
 
-export type TemplateId = 'kibana' | 'security';
+export type TemplateId = 'kibana' | 'security' | 'endpoint';
 
 export interface TemplateInfo {
   id: TemplateId;
   name: string;
-  icon: string;
+  icon: EuiIconProps['type'];
   config: Config;
 }
 
 export const templates: TemplateInfo[] = [
   { id: 'kibana', name: 'Kibana', icon: 'logoKibana', config: kibanaTemplate },
   { id: 'security', name: 'Security', icon: 'logoSecurity', config: securityTemplate },
+  { id: 'endpoint', name: 'Endpoint', icon: 'logoElastic', config: endpointTemplate },
 ];
