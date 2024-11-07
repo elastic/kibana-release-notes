462bd8448050ab2c4271f932a87e094ad79c8ae4
Brad White <brad.white@elastic.co>
2024-11-06 17:33:49 -0700

    add serverless template

--- src/config/templates/index.ts
+++ src/config/templates/index.ts
@@ -1,12 +1,13 @@
 import { kibanaTemplate } from './kibana';
 import { securityTemplate } from './security';
 import { endpointTemplate } from './endpoint';
+import { serverlessTemplate } from './serverless';
 import { Config } from './types';
 import type { EuiIconProps } from '@elastic/eui';
 
 export * from './types';
 
-export type TemplateId = 'kibana' | 'security' | 'endpoint';
+export type TemplateId = 'kibana' | 'security' | 'endpoint' | 'serverless';
 
 export interface TemplateInfo {
   id: TemplateId;
@@ -19,4 +20,5 @@ export const templates: TemplateInfo[] = [
   { id: 'kibana', name: 'Kibana', icon: 'logoKibana', config: kibanaTemplate },
   { id: 'security', name: 'Security', icon: 'logoSecurity', config: securityTemplate },
   { id: 'endpoint', name: 'Endpoint', icon: 'logoElastic', config: endpointTemplate },
+  { id: 'serverless', name: 'Serverless', icon: 'logoKibana', config: serverlessTemplate },
 ];
