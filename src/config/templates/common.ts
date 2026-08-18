7bd7cfa5ae829c9ee51aa035904195be6833aaa6
Brad White <Ikuni17@users.noreply.github.com>
2026-08-18 11:38:16 -0600

    Apply suggestion from @Ikuni17

--- src/config/templates/common.ts
+++ src/config/templates/common.ts
@@ -306,7 +306,7 @@ export const kibanaAreas: AreaDefinition[] = [
       'Team:ResponseOps',
       'Team:Cases',
       'Feature:Cases',
-      'Feature:AlertingV2'
+      'Feature:AlertingV2',
     ],
   },
   {
